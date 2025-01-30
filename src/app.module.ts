f0f0ee1b33940e94b4ae92c311d93eccae83223a
juanquintero97 <juanqg1997@gmail.com>
2025-01-30 18:09:00 -0500

    Using config and dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,16 +11,19 @@ import { Cities } from './cities/entities/cities.entity/cities.entity';
 import { CountriesService } from './countries/countries.service';
 import { StatesService } from './states/states.service';
 import { CitiesService } from './cities/cities.service';
+import { ConfigModule } from '@nestjs/config';
+import { parseString } from 'fast-csv';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'dbadmin',
-      password: 'password',
-      database: 'locations_api',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '5432'),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Countries, States, Cities],
       synchronize: true
     }),
